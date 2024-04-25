2cd684e671a4eafce3ea72970470199676d9a583
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2024-04-25 09:12:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,79 @@
+let menu = document.querySelector('#menu-bars');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+let themeToggler = document.querySelector('.theme-toggler');
+let toggleBtn = document.querySelector('.toggle-btn');
+toggleBtn.onclick = () =>{
+    themeToggler.classList.toggle('active');
+}
+
+document.querySelectorAll('.theme-toggler .theme-btn').forEach(btn =>{
+    
+    btn.onclick = () =>{
+        let color = btn.style.background;
+        document.querySelector(':root').style.setProperty('--main-color',color);
+    }
+})
+
+
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    themeToggler.classList.remove('active');
+}
+
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+    },
+   loop: true,
+   autoplay: {
+    delay:3000,
+    disableOnInteraction:false,
+   }
+  });
+
+//Like button 
+function likeImage(){
+    var currentLikes = parseInt(button.innerText) || 0;
+    button.innerText = currentLikes + 1;
+    // Add logic to send this information to a server or store it if needed
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//color randomizer
